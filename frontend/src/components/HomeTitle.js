a3e2cc97b0541244590d5ff31ade8c1d36934ded
jared-baker99 <j.baker.99@knights.ucf.edu>
2023-03-14 02:24:17 -0400

    test to iron out the kinks to start design

---
+++ frontend/src/components/HomeTitle.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function HomeTitle()
+{
+    return(
+        <h1 id='title'>RECIPEASY</h1>
+    );
+};
+
+export default HomeTitle;
---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import HomeTitle from '../components/HomeTitle';
+
+const HomePage = () =>
+{
+    return(
+        <div>
+            <HomeTitle />
+        </div>
+    );
+};
+
+export default HomePage;
