18e03f95ba5c2828e7aedecf7429c6ce7cd6bd44
jared-baker99 <j.baker.99@knights.ucf.edu>
2023-03-15 23:13:41 -0400

    made changes to the background color trying to get the layout like the figma.

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -10,16 +10,16 @@ function HomeTitle()
     };
 
     return(
-        <div>
+        <div class="background">
             <div class="title-Bar">
                 <h1 id='title'>RECIPEASY</h1>
-                <button onClick={Click} class="App-link">Click Me</button>
+                <button onClick={Click} class="text">Click Me</button>
             </div>
             <div class="pan-gro">
-
+                <h1 id='title'>Howdy</h1>
             </div>
             <div class="recipe-List">
-
+                <h1 id='title'>Partner</h1>
             </div>
         </div>
     );
