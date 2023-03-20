3cb7e65bbd1bfc69452d8c1aa3a118d7b081fcc0
jared-baker99 <j.baker.99@knights.ucf.edu>
2023-03-20 01:10:50 -0400

    adding more divs to format the page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -6,23 +6,43 @@ function HomeTitle()
     {
         event.preventDefault();
         
-        alert('doIt()');
+        alert('settings()');
     };
+
+    //pan = pantry
+    //gro = grocery
 
     return(
         <div class="background">
-            <div class="title-bar">
+            <div class="title-bar"> 
                 <h1 id='title'>RECIPEASY</h1>
                 <div class="setting-button">
                     <button onClick={Click} class="text">SETTING BUTTON</button>
                 </div>
             </div>
-            <div class="content">
+            <div class="content"> 
                 <div class="pan-gro">
+                    <div class="pan-gro-ui">
+                        <div class="pan-button">
+                            <h1 class='title'>PANTRY</h1>
+                        </div>
+                        <div class="gro-button">
+                            <h1 class='title'>GROCERY</h1>
+                        </div>
+                        <div class="pan-gro-item">
 
+                        </div>
+                    </div>
                 </div>
                 <div class="recipe">
-                    
+                    <div class="recipe-ui">
+                        <div class="recipe-header">
+                            <h1 class='title'>RECIPES</h1>
+                        </div>
+                        <div class="recipe-item">
+
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
