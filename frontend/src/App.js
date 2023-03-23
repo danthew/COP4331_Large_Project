7f4bd2c248211b207b41985dc92583458e2c3688
jared-baker99 <j.baker.99@knights.ucf.edu>
2023-03-23 17:51:03 -0400

    working on the login html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 
 import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 
 function App() 
 {
   return (
-    <HomePage />
+    <LoginPage />
   );
 }
 
--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,15 +1,46 @@
 import React from 'react';
 
-import PageTitle from '../components/PageTitle';
 import Login from '../components/Login';
 import Register from '../components/Register';
 
+const loginButton = async event =>
+    {   
+        event.preventDefault();
+        document.getElementById("register").style.display = "none";
+        document.getElementById("login").style.display    = "block";
+
+        //alert('grocery()');
+    };
+
+const registerButton = async event =>
+    {
+        event.preventDefault();
+        document.getElementById("login").style.display = "none";
+        document.getElementById("register").style.display = "block";
+
+        //alert('grocery()');
+    };
+
 const LoginPage = () => {
     return (
-        <div>
-            <PageTitle />
-            <Login />
-            <Register />
+        <div class="background"> 
+            <div id="logo-login">
+                LOGO
+            </div>
+            <div class="login-div"> 
+                <div class="login-button" onClick={loginButton}>
+                    login
+                </div>
+                <div class="register-button" onClick={registerButton}>
+                    register
+                </div>
+                <div class="login" id="login">
+                    <Login />
+                </div>
+                <div class="register" id="register">
+                    <Register />
+                </div>
+            </div>
         </div>
     );
 };
