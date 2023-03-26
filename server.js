2f015eca26bf4983935c3fe0759476e8852b9010
Daniel Thew <80126553+danthew@users.noreply.github.com>
2023-03-26 07:38:06 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 const app = express();
+
+app.set('port', (process.env.PORT || 5000));
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -23,4 +27,8 @@ app.use((req, res, next) =>
     next();
 });
 
-app.listen(process.env.PORT || 5000); // start Node + Express server on port 5000
+app.listen(PORT, ()=>
+{
+    console.log('Server listening on port ' + PORT);
+}
+); // start Node + Express server on port 5000
