f77f8e3b6df3a930514f01fabca9464566d46971
Daniel Thew <80126553+danthew@users.noreply.github.com>
2023-03-26 07:41:38 -0400

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,17 @@ app.set('port', (process.env.PORT || 5000));
 app.use(cors());
 app.use(bodyParser.json());
 
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production')
+{
+// Set static folder
+app.use(express.static('frontend/build'));
+app.get('*', (req, res) =>
+{
+res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+});
+}
+
 app.use((req, res, next) =>
 {
     res.setHeader('Access-Control-Allow-Origin', '*');
