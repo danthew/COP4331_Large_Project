7ca881725ae0c582c5de0330e6b4960b6ab54ce2
Daniel Thew <80126553+danthew@users.noreply.github.com>
2023-03-26 12:09:18 -0400

    Update Login.js

--- frontend/src/components/home/login/Login.js
+++ frontend/src/components/home/login/Login.js
@@ -49,7 +49,7 @@ function Login() {
                 method: "POST",
                 body: js,
                 headers: { "Content-Type": "application/json", 
-                            "Access-Control-Allow-Origin" : "no-cors",
+                            "Access-Control-Allow-Origin" : "https://localhost:5001" || "https://recipeasy123.herokuapp.com",
                             "Access-Control-Allow-Methods" : "POST"},
             });
             var res = JSON.stringify(response.body);
