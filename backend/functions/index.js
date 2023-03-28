db0463497ec497248a492a63ada57d9411328109
tylerm390 <77948992+tylerm390@users.noreply.github.com>
2023-03-27 20:58:32 -0400

    Repush

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -29,9 +29,8 @@ app.use((req, res, next) => {
     'Origin, X-Requested-With, Content-Type, Accept, Authorization' );
     res.setHeader(
     'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS'
-);
-next();
+    'GET, POST, PATCH, DELETE, OPTIONS');
+    next();
 });
 
 const fb = require('firebase/app');
