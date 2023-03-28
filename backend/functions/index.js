8f01d741c9bbc7245b843455e80d17297e98c175
tylerm390 <77948992+tylerm390@users.noreply.github.com>
2023-03-27 21:01:13 -0400

    Update index.js

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -25,11 +25,13 @@ const firebaseConfig = {
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization' );
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization' 
+    );
     res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS');
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, OPTIONS'
+    );
     next();
 });
 
