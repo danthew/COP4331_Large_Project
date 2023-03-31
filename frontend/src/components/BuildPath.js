74fb8702cdc6486b1cac81917450d32283df21a7
Jared Baker <j.baker.99@knights.ucf.edu>
2023-03-31 18:07:08 -0400

    404 error?

--- frontend/src/components/BuildPath.js
+++ frontend/src/components/BuildPath.js
@@ -1,7 +1,7 @@
 exports.buildPath =
 function buildPath(route) {
     if(process.env.PORT === 'production') {
-        return 'https://recipeasy123.herokuapp.com/' + route;
+        return 'https://us-central1-recipeasy-ec759.cloudfunctions.net/' + route;
     }
     else {
         return 'http://localhost:5001/recipeasy-ec759/us-central1/' + route;
