86168d32591aa39835ec44977a610a1e0e1ce272
Jared Baker <j.baker.99@knights.ucf.edu>
2023-03-31 18:10:24 -0400

    404?

--- frontend/src/components/BuildPath.js
+++ frontend/src/components/BuildPath.js
@@ -1,6 +1,6 @@
 exports.buildPath =
 function buildPath(route) {
-    if(process.env.PORT === 'production') {
+    if(process.env.NODE_ENV === 'production') {
         return 'https://us-central1-recipeasy-ec759.cloudfunctions.net/' + route;
     }
     else {
