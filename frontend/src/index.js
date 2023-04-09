90555613563fa5835176202231dd2f11a8866a5b
jared-baker99 <j.baker.99@knights.ucf.edu>
2023-04-08 22:21:14 -0400

    Revert "deleted server.js"
    This reverts commit c8f58bf00a85b8f88ff2fbf6869e8b831044d95f.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,19 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import firebase from "firebase";
-
-firebase.initilizeApp = {
-  apiKey: "AIzaSyCLxtVg_Ad5YNVGQHuecjYdIYXyzm4RrUg",
-  authDomain: "recipeasy-ec759.firebaseapp.com",
-  databaseURL: "https://recipeasy-ec759-default-rtdb.firebaseio.com",
-  projectId: "recipeasy-ec759",
-  storageBucket: "recipeasy-ec759.appspot.com",
-  messagingSenderId: "292406285745",
-  appId: "1:292406285745:web:022fe474eb1ad249035990",
-  measurementId: "G-9X7Z2ZH6V7"
-};
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
