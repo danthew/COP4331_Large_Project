11287d46ed54892dbc6deb44506c44a9c1cd2429
joshgumprecht <106719554+joshgumprecht@users.noreply.github.com>
2023-04-21 16:23:00 -0400

    Update Recipe.js

--- frontend/src/components/home/Recipe.js
+++ frontend/src/components/home/Recipe.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useCookies } from 'react-cookie';
 
-function Recipe()
-{
-    var recipeSearch = '';
+function Recipe() {
+  const [recipes, setRecipes] = useState([]);
+  const [cookie, setCookie] = useCookies(['userId']);
+
+  var recipeSearch = '';
     const [message,setMessage] = useState('');
 
     const doSearchRecipe = async event =>
@@ -19,26 +22,56 @@ function Recipe()
         window.location.href = "/addRecipe";
     }
 
-    return(
-        <div className="recipe">
-            <div className="recipe-ui">
-                <div className="recipe-header">
-                    <h1 className='label'> Recipes </h1>
-                </div>
-                <div className="search">
-                    <form className="search">
-                        <button className="add-Recipe" id='addRecipe' onClick={addRecipe}>+</button>
-                        <input className="search-bar" id="recipeSearch" placeholder="Search Recipes" ref={(c) => recipeSearch = c}/>
-                        <button className="search-button" onClick={doSearchRecipe}> Search </button>
-                        <span id="recipeSearch">{message}</span>
-                    </form>
-                </div>
-                <div className="recipe-item">
-                    
-                </div>
-            </div>
+  const listRecipes = async () => {
+    const userId = cookie.userId;
+
+    try {
+      const response = await fetch('http://localhost:5001/recipeasy-ec759/us-central1/api/listRecipes', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ userId }),
+      });
+      const data = await response.json();
+      setRecipes(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    listRecipes();
+  }, []);
+
+  return (
+    <div className="recipe">
+      <div className="recipe-ui">
+        <div className="recipe-header">
+          <h1 className="label"> Recipes </h1>
         </div>
-    );
+        <div className="search">
+            <form className="search">
+                <button className="add-Recipe" id='addRecipe' onClick={addRecipe}>+</button>
+                <input className="search-bar" id="recipeSearch" placeholder="Search Recipes" ref={(c) => recipeSearch = c}/>
+                <button className="search-button" onClick={doSearchRecipe}> Search </button>
+                <span id="recipeSearch">{message}</span>
+            </form>
+        </div>
+        <div className="recipe-items">
+          {recipes.map(recipe => (
+            <div className="recipe-item" key={recipe.recipeId}>
+              <p>Recipe Name: {recipe.name}</p>
+              <p>Recipe Cuisine: {recipe.cuisine}</p>
+              <p>Prep Time: {recipe.prepTime}</p>
+              <p>Cook Time: {recipe.cookTime}</p>
+              <p>Allow Subs?: {recipe.allowSubs}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Recipe;
