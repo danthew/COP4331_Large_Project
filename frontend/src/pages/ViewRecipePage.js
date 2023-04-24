65fdaa95afb82daf9f2a9f24ba5c384f3116b772
joshgumprecht <106719554+joshgumprecht@users.noreply.github.com>
2023-04-24 01:45:03 -0400

    Create ViewRecipePage.js

---
+++ frontend/src/pages/ViewRecipePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import ViewRecipe from 'components/home/ViewRecipe';
+
+const ViewRecipePage = () =>
+{
+    return(
+        <div>
+            <ViewRecipe />
+        </div>
+    );
+};
+
+export default ViewRecipePage;
