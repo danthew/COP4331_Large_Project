43795c9b4917b4f64049151d72245e177a4c8748
joshgumprecht <106719554+joshgumprecht@users.noreply.github.com>
2023-04-24 01:44:00 -0400

    Update AddRecipe.js

--- frontend/src/components/home/addRecipe/AddRecipe.js
+++ frontend/src/components/home/addRecipe/AddRecipe.js
@@ -1,5 +1,7 @@
+
 import React, { useState } from "react";
 import { useCookies } from 'react-cookie';
+import Logo from 'images/logo.png';
 
 function AddRecipe() {
 
@@ -8,13 +10,15 @@ function AddRecipe() {
     const[stepsList, setstepsList] = useState([]);
     const[number, setNumber] = useState([]);
     const [message, setMessage] = useState("");
-    const [cookie, setCookie] = useCookies(['userId', 'recipeId', 'ingredientId']);
+    const [cookie, setCookie] = useCookies(['userId', 'recipeId', 'instructionId', 'ingredientId']);
+    const [allowSubs, setallowSubs] = useState([]);
 
     var name;
     var cookTime;
     var cuisine;
     var prepTime;
 
+    const app_name = 'recipeasy123'
 	function buildPath(route)
 	{
 		if (process.env.NODE_ENV === 'production')
@@ -70,7 +74,7 @@ function AddRecipe() {
         setNumber(ip);
     }
     const handleClick=(cb)=>{
-        console.log(cb.checked);
+        console.log(cb.checked); 
     }
     //console.log(ingredientList, 'data-');
 
@@ -83,34 +87,18 @@ function AddRecipe() {
             cuisine: cuisine.value,
             cookTime: cookTime.value, 
             prepTime: prepTime.value,
-            allowSubs: "false",
+            allowSubs: allowSubs,
             userId: cookie.userId
         };
 
-        let obj2 = {
-            name: ingredientList,
-            quantity: quantity,
-            recipeId: cookie.recipeId
-        }
 
-        let obj3 = {
-            body: stepsList,
-            stepNumber: number,
-            recipeId: cookie.recipeId
-        }
 
         let js = JSON.stringify(obj); 
         console.log(JSON.stringify(obj,null,2));
 
-        let js2 = JSON.stringify(obj2);
-
-
-        let js3 = JSON.stringify(obj3);
-        console.log(JSON.stringify(obj3,null,2));
-
 
         try {
-            const response = await fetch(buildPath('api/addRecipe'), {
+            const response = await fetch('http://localhost:5001/recipeasy-ec759/us-central1/api/addRecipe', {
                 method: "POST",
                 body: js,
                 headers: { Accept: 'application/json',
@@ -133,8 +121,17 @@ function AddRecipe() {
             console.log(e);
             return;
         }
+
+        let obj2 = {
+            name: ingredientList,
+            quantity: quantity,
+            recipeId: res.recipeId
+        }
+
+        let js2 = JSON.stringify(obj2);
+
         try {
-            const response = await fetch(buildPath('api/addIngredientToRecipe'), {
+            const response = await fetch('http://localhost:5001/recipeasy-ec759/us-central1/api/addIngredientToRecipe', {
                 method: "POST",
                 body: js2,
                 headers: { Accept: 'application/json',
@@ -147,6 +144,7 @@ function AddRecipe() {
             else {
                 var res2 = JSON.parse(await response.text());
                 setCookie(' ingredientId', res2.ingredientId, {path: '/'});
+                console.log(cookie.ingredientId);
                 setMessage('');
                 window.location.href = "/home";
             }
@@ -157,8 +155,18 @@ function AddRecipe() {
             console.log(e);
             return;
         }
+
+        let obj3 = {
+            body: stepsList,
+            stepNumber: number,
+            recipeId: res.recipeId
+        }
+
+        let js3 = JSON.stringify(obj3);
+        console.log(JSON.stringify(obj3,null,2));
+
         try {
-            const response = await fetch(buildPath('api/addInstruction'), {
+            const response = await fetch('http://localhost:5001/recipeasy-ec759/us-central1/api/addInstruction', {
                 method: "POST",
                 body: js3,
                 headers: { Accept: 'application/json',
@@ -183,70 +191,79 @@ function AddRecipe() {
         }
     }
 
-    const cancel = async (event) =>
-    {
-        event.preventDefault();
-        window.location.href = "/home";
-    }
-
     return (
         <div className="background">
+            <div className="title">
+                <div className="text"> 
+                    <h1 className='title'>recipeasy</h1> 
+                 
+                </div>
+                <div className ="logo">  
+                        <img height="40px" width="40px" src={Logo} alt="RECIPEASY Logo"></img>
+                </div> 
+            </div>
             <div class="add-recipe">
-                <div className="rec-title">
-                    <label>Recipe Title</label>
-                    <input className="recipe-title" size="10" id="name" placeholder="Title" ref={(c) => name = c}/>
-                </div>
-                <div className="add-Ing">
-                    <hr color="#337AB7" size="5" width="100%"></hr>
-                    {ingredientList.map((data, i)=> {
-                        return(
-                            <div class="ingredients">
-                                    <form>
-                                        <label>Quantity :</label>
-                                        <input className="quantity" size="5" onChange={ e =>handleinputchangeQ(e, i)}></input>
-                                        <label>Ingredient :</label>
-                                        <input className="ingredient" size="15" onChange={ e =>handleinputchange(e, i)}></input> 
-                                        <button className="X" onClick={()=>handleRemove(i)}>x</button>  
-                                    </form>
+            <div className="rec-title">
+                <label>Recipe Title</label>
+                <input className="recipe-title" id="name" placeholder="Title" ref={(c) => name = c}/>
+            </div>           
+            <div class="add-Ing">
+                <hr color="#337AB7" size="5" width="100%"></hr>
+                {ingredientList.map((data, i)=> {
+                    return(
+                        <div class="ingredients">
+                                
+                                <label>Ingredient</label>
+                                <input name="ingredient" onChange={ e =>handleinputchange(e, i)}></input> 
+                                <label>Quantity</label>
+                                <input  name="quantity"onChange={ e =>handleinputchangeQ(e, i)}></input>
+                                <button onClick={()=>handleRemove(i)}>x</button>  
+                        </div>
+                    );  
+                })}
+                <button className="btn btn-add" onClick={()=>addField()}>Add Ingredient</button>             
+            </div>
+            <div class="cook-time">
+                <hr color="#337AB7" size="5" width="100%"></hr>
+                <label>Cook Time</label>
+                <input type='number' id='cookTime' placeholder="Cook Time" ref={(c) => cookTime = c}></input> 
+            </div>
+            <div class="prep-time">
+                <hr color="#337AB7" size="5" width="100%"></hr>
+                <label>Prep Time</label>
+                <input type='number' id='prepTime' placeholder="Prep Time" ref={(c) => prepTime = c}></input> 
+            </div>
+            <div class="cuisine">
+                <label>Cuisine</label>
+                <input type='text' id='cuisine' placeholder="Cuisine" ref={(c) => cuisine = c}></input> 
+            </div>
+            <div class="addsubs">
+                <label>Allow Substitutions?</label>
+                <input type="radio" name="radio" id="opt1" onChange={()=> setallowSubs(true)}></input>
+                <label for="opt1" class="label1">
+                    <span>Yes</span>
+                </label>
+                <input type="radio" name="radio" id="opt2" onChange={()=> setallowSubs(false)}></input>
+                <label for="opt2" class="label2">
+                    <span>No</span>
+                </label>
+            </div>
+            <div class="add-delete">                       
+                {stepsList.map((data, i)=> {
+                    return (
+                        <div class="ingredients">
+                            <div class="addStep">
+                                <label>Steps</label>
+                                <input value={data} onChange={ e =>handleStepChange(e,i)}></input>
+                                <button onClick={()=>removeStep(i)}>x</button>  
                             </div>
-                        );  
-                    })}
-                    <hr color="#337AB7" size="5" width="100%"></hr>
-                    <button className="btn btn-add" onClick={()=>addField()}>Add Ingredient</button>             
-                </div>
-                <div className="cook-time">
-                    <label className="cook">Cook Time :</label>
-                    <input type='number' id='cookTime' placeholder="Cook Time" ref={(c) => cookTime = c}></input> 
-                    <label className="time">min</label>
-                </div>
-                <div className="prep-time">
-                    <label>Prep Time :</label>
-                    <input type='number' id='prepTime' placeholder="Prep Time" ref={(c) => prepTime = c}></input> 
-                    <label className="time">min</label>
-                </div>
-                <div className="cuisine">
-                    <label>Cuisine :</label>
-                    <input type='text' id='cuisine' placeholder="Cuisine" ref={(c) => cuisine = c}></input> 
-                </div>
-                <div class="add-delete">
-                    <hr color="#337AB7" size="5" width="100%"></hr>
-                    {stepsList.map((data, i)=> {
-                        return (
-                            <div className="ingredients">
-                                <div className="addStep">
-                                    <label>Steps</label>
-                                    <input className="step" value={data} onChange={ e =>handleStepChange(e,i)}></input>
-                                    <button className="X" onClick={()=>removeStep(i)}>x</button>  
-                                </div>
-                            </div>
-                        );
-                    })}
-                    <hr color="#337AB7" size="5" width="100%"></hr>
-                    <button className="btn-add" onClick={()=>addStep()}>Add Step</button>
-                </div>
-                <input type="submit" id="addRecipeButton" className="buttons" value="AddRecipe" onClick={addRecipe}/>
-                <input type="submit" id="cancel" className="buttons" value="Cancel" onClick={cancel} />
-                <span id="addRecipeResult">{message}</span>
+                        </div>
+                    );
+                })}
+                <button className="btn btn-add" onClick={()=>addStep()}>Add Step</button>
+            </div>
+            <input type="submit" id="addRecipeButton" className="buttons" value="AddRecipe" onClick={addRecipe}/>
+            <span id="addRecipeResult">{message}</span>
             </div>
         </div>
     )
