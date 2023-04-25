9b58a0fbcf0f7a7e9a2553f571282b6439215796
Jared Baker <j.baker.99@knights.ucf.edu>
2023-04-24 22:44:51 -0400

    forgot password is implemented

--- frontend/src/components/ForgotPassword.js
+++ frontend/src/components/ForgotPassword.js
@@ -1,16 +1,61 @@
 import React, {useState} from 'react';
 import { useCookies } from 'react-cookie';
 
-var forgotPass;
+
 
 function Login() {
     const [message, setMessage] = useState("");
+    var forgotPass;
+    
+    function buildPath(route)
+	{
+		if (process.env.NODE_ENV === 'production')
+		{
+		return 'https://us-central1-recipeasy-ec759.cloudfunctions.net/' + route;
+		}
+		else
+		{
+		return 'http://localhost:5001/recipeasy-ec759/us-central1/' + route;
+		}
+	}
 
     const doForgot = async event => {
         event.preventDefault();
 
-        alert('' +forgotPass);
-    }
+        var obj = {
+            email : forgotPass.value
+        };
+
+        var js = JSON.stringify(obj);
+
+        try{
+            const response = await fetch(buildPath('api/resetPassword'), {
+                method: 'POST',
+                body: js,
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+            });
+
+            var res = JSON.parse(await response.text());
+
+            if(res.id <= 0)
+            {
+                setMessage('Email is Incorrect');
+            }
+            else
+            {
+                window.location.href = "/";
+            }
+        }
+        catch(e)
+        {
+            alert(e.toString());
+            return;
+        }
+
+        //alert(forgotPass.value);
+    };
  
     return (
         <div>
