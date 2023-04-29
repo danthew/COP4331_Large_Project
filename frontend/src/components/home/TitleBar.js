125f85361fb8b3a6d71ba701897b28518bb56195
Daniel Thew <80126553+danthew@users.noreply.github.com>
2023-04-29 07:50:27 -0400

    Add files via upload

--- frontend/src/components/home/TitleBar.js
+++ frontend/src/components/home/TitleBar.js
@@ -65,7 +65,7 @@ function HomeTitle()
                 <span className="welcome-User">Welcome {name}</span><br/>
             </div>
             <div className="setting-button">
-                <button onClick={dropdown} className="setting-drop">SETTING BUTTON</button>
+                <button onClick={dropdown} className="setting-drop">SETTINGS</button>
                 <div id="dropdown-content" className='dropdown-content'>
                     <button onClick={profile} className="drop-button" id="profile">Profile</button>
                     <button onClick={logout} className="drop-button" id="logout">Logout</button>
